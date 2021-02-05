9a43a7561df09fd080af99d05030f0517010c0f6
Sone <sthaya@gmail.com>
2021-02-04 17:32:52 -0800

    started netflix stripe clone project, added firebase config file and delete files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCL9zN41lGRVu9Iz5YvPEFLJpYI3v0Qy4w",
+    authDomain: "netflix-clone-7485c.firebaseapp.com",
+    projectId: "netflix-clone-7485c",
+    storageBucket: "netflix-clone-7485c.appspot.com",
+    messagingSenderId: "245398867899",
+    appId: "1:245398867899:web:b92ac3fe36e84188bc7e9e"
+  };
