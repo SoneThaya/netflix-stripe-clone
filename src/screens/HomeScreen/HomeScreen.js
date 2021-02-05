7526b4c028c53391a1991a11b9114559a4407fb0
Sone <sthaya@gmail.com>
2021-02-04 18:31:27 -0800

    created nav bar component and styled, setup scroll transition

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../../components/Nav/Nav";
+import "./HomeScreen.css";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      <Nav />
+    </div>
+  );
+};
+
+export default HomeScreen;
