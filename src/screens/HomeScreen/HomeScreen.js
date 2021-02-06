35f362fa2ab9ca66a892b6d8ad78ae3cc2067cce
Sone <sthaya@gmail.com>
2021-02-05 16:27:57 -0800

    styled play and my list buttons, created truncate function to limit desicription field

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Banner from "../../components/Banner/Banner";
 import Nav from "../../components/Nav/Nav";
 import "./HomeScreen.css";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
-      <Nav />
+          <Nav />
+          <Banner />
     </div>
   );
 };
