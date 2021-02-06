ea4400eec087c567075027213209fc792bfbb48f
Sone <sthaya@gmail.com>
2021-02-06 14:29:16 -0800

    created login screen, installed reat router dom refactored app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
 const App = () => {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+          </Switch>
+        )}
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
