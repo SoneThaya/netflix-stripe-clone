e916770eadc180be3418d96ef3502998493226a4
Sone <sthaya@gmail.com>
2021-02-06 18:55:24 -0800

    set up firbase login and sign up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import { auth } from "./firebase";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 
 const App = () => {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // logged in
+        console.log(userAuth);
+      } else {
+        // logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCL9zN41lGRVu9Iz5YvPEFLJpYI3v0Qy4w",
-    authDomain: "netflix-clone-7485c.firebaseapp.com",
-    projectId: "netflix-clone-7485c",
-    storageBucket: "netflix-clone-7485c.appspot.com",
-    messagingSenderId: "245398867899",
-    appId: "1:245398867899:web:b92ac3fe36e84188bc7e9e"
-  };
+  apiKey: "AIzaSyCL9zN41lGRVu9Iz5YvPEFLJpYI3v0Qy4w",
+  authDomain: "netflix-clone-7485c.firebaseapp.com",
+  projectId: "netflix-clone-7485c",
+  storageBucket: "netflix-clone-7485c.appspot.com",
+  messagingSenderId: "245398867899",
+  appId: "1:245398867899:web:b92ac3fe36e84188bc7e9e",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
